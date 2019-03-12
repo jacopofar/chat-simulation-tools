55870118eccb1bd2dcf7311a17d7103ebf378b57
Jacopo Farina <jacopo.farina@flixbus.com>
2019-03-12 13:19:17 +0100

    Switch to grid layout for the message

--- visualz/frontend/src/ChatMessage.js
+++ visualz/frontend/src/ChatMessage.js
@@ -9,12 +9,12 @@ function ChatMessage({ message }) {
   return (
     <div className="chat__message-box">
     <span className="chat__message-user">{message.user_print_name}</span>
-    <time dateTime={message.timestamp}>{message.timestamp}</time>
-    <p>{message.text}</p>
+    <time className="chat__message-timestamp" dateTime={message.timestamp}>{message.timestamp}</time>
+    <button className="chat__message-analysis-deptree" onClick={toggleDepTree}>Dependency tree</button>
+    <p className="chat__message-text">{message.text}</p>
     {showDeptree &&
-      <img className="chat__message-analysis" src={"/analyze?text=" + message.text + "&style=dep"}/>
+      <img className="chat__message-analysis" alt="dependency tree for the text" src={"/analyze?text=" + message.text + "&style=dep"}/>
     }
-    <span onClick={toggleDepTree}>[Dependency tree]</span>
     </div>
     );
   }
